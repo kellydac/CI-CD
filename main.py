eab319ac0c227573899aad245e1823c4abbf5f4c
kellydac <dacanaykc20@uww.edu>
2025-09-29 12:30:18 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # A simple greeting
+    return "Hello, World! The CI/CD Pipeline worked! Version 2.0"
