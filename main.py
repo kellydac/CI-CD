d89e522d4664a808f608ed63016d33461e11328b
kellydac <dacanaykc20@uww.edu>
2025-09-29 14:00:38 -0500

    Added port cloud run assign

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # main.py
 
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
     return "Hello, World! The CI/CD Pipeline worked! Version 2.0"
 
-# (no need for __main__ block when using gunicorn)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
